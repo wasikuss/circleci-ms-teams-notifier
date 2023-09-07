5b65c4a1a50c30be34d4c5d557f2af5684659893
wasikuss <wasikuss@gmail.com>
2023-09-08 00:47:04 +0200

    tmp

--- src/scripts/env.sh
+++ src/scripts/env.sh
@@ -1,3 +1,3 @@
-echo 'export WEBHOOK_URL="<< parameters.webhook_url >>"' >> "$BASH_ENV"
-echo 'export SHOW_MSG="<< parameters.show_msg >>"' >> "$BASH_ENV"
-echo 'export SHOW_AUTHOR="<< parameters.show_author >>"' >> "$BASH_ENV"
+echo 'export WEBHOOK_URL="\<< parameters.webhook_url >>"' >> "$BASH_ENV"
+echo 'export SHOW_MSG="\<< parameters.show_msg >>"' >> "$BASH_ENV"
+echo 'export SHOW_AUTHOR="\<< parameters.show_author >>"' >> "$BASH_ENV"
