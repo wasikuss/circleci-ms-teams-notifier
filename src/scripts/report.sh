1e67649b4fdbb1d7805ece507be5378ab042284a
wasikuss <wasikuss@gmail.com>
2023-10-20 07:20:31 +0200

    tmp

--- src/scripts/report.sh
+++ src/scripts/report.sh
@@ -1,8 +1,11 @@
-set -x
+
 
 MSG_PATH=/tmp/ms_teams_replaced_message
 
 sed -e 's/__build_status__/'$STATUS'/' -e 's/__build_color__/'$COLOR'/' \
     -e 's/__build_img__/'$IMG'/' /tmp/ms_teams_message > $MSG_PATH
+
+echo $MSG_PATH
+
 curl --fail -H "Content-Type: application/json" --data-binary @$MSG_PATH \
     $MS_TEAMS_WEBHOOK_URL
