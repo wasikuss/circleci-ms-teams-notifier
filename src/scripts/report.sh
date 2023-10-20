3466d4335c5a95e86cbca7f28a0246357857321c
wasikuss <wasikuss@gmail.com>
2023-10-20 06:58:28 +0200

    tmp

--- src/scripts/report.sh
+++ src/scripts/report.sh
@@ -1,6 +1,6 @@
 MSG_PATH=/tmp/ms_teams_replaced_message
 
-sed -e 's/__build_status__/'$STATUS'/' -e 's/__theme_color__/'$COLOR'/' \
-    /tmp/ms_teams_message > $MSG_PATH
+sed -e 's/__build_status__/'$STATUS'/' -e 's/__build_color__/'$COLOR'/' \
+    -e 's/__build_img__/'$IMG'/' /tmp/ms_teams_message > $MSG_PATH
 curl --fail -H "Content-Type: application/json" --data-binary @$MSG_PATH \
     $MS_TEAMS_WEBHOOK_URL
--- src/scripts/template.sh
+++ src/scripts/template.sh
@@ -16,41 +16,101 @@ else
 fi
 
 if [[ $SHOW_AUTHOR == "1" ]]; then
-    COMMIT_AUTHOR=`git log --format="%an <%ae>" -1 $CIRCLE_SHA1`
+    COMMIT_AUTHOR_NAME=`git log --format="%an <%ae>" -1 $CIRCLE_SHA1`
+    COMMIT_AUTHOR_MAIL=`git log --format="%ae" -1 $CIRCLE_SHA1`
 else
-    COMMIT_AUTHOR="n/a"
+    COMMIT_AUTHOR_NAME="n/a"
+    COMMIT_AUTHOR_MAIL="n/a"
 fi
 
 MS_TEAMS_MSG_TEMPLATE=$(cat <<END_HEREDOC
 {
-    "@type": "MessageCard",
-    "@context": "http://schema.org/extensions",
-    "themeColor": "__theme_color__",
-    "summary": "CircleCI Build Notification",
-    "sections": [
+    "type": "AdaptiveCard",
+    "body": [
         {
-            "activityTitle": "__build_status__: $CIRCLE_PROJECT_REPONAME job [${CIRCLE_JOB} #${CIRCLE_BUILD_NUM}]($CIRCLE_BUILD_URL)",
-            "facts": [
+            "type": "Container",
+            "bleed": true,
+            "backgroundImage": {
+                "url": "__build_img__"
+            }
+        },
+        {
+            "type": "Container",
+            "items": [
                 {
-                    "name": "Git ref",
-                    "value": "$CIRCLE_BRANCH $CIRCLE_TAG"
+                    "type": "ColumnSet",
+                    "columns": [
+                        {
+                            "type": "Column",
+                            "items": [
+                                {
+                                    "type": "TextBlock",
+                                    "text": "STATUS:",
+                                    "weight": "Bolder"
+                                }
+                            ],
+                            "width": "auto",
+                            "horizontalAlignment": "Left"
+                        },
+                        {
+                            "type": "Column",
+                            "items": [
+                                {
+                                    "type": "TextBlock",
+                                    "text": "__build_status__",
+                                    "weight": "Bolder",
+                                    "color": "__build_color__"
+                                }
+                            ],
+                            "width": "auto",
+                            "spacing": "Small"
+                        }
+                    ]
                 },
                 {
-                    "name": "Commit",
-                    "value": "$COMMIT_LINK"
+                    "type": "TextBlock",
+                    "text": "$CIRCLE_PROJECT_REPONAME job [${CIRCLE_JOB} #${CIRCLE_BUILD_NUM}]($CIRCLE_BUILD_URL)",
+                    "weight": "Bolder"
                 },
                 {
-                    "name": "Message",
-                    "value": "$COMMIT_MSG"
-                },
-                {
-                    "name": "Author",
-                    "value": "$COMMIT_AUTHOR"
+                    "type": "FactSet",
+                    "facts": [
+                        {
+                            "title": "Git ref",
+                            "value": "$CIRCLE_BRANCH $CIRCLE_TAG"
+                        },
+                        {
+                            "title": "Commit",
+                            "value": "$COMMIT_LINK"
+                        },
+                        {
+                            "title": "Message",
+                            "value": "$COMMIT_MSG"
+                        },
+                        {
+                            "title": "Author",
+                            "value": "<at>$COMMIT_AUTHOR_NAME</at>"
+                        }
+                    ]
                 }
             ],
-            "markdown": true
+            "bleed": true
         }
-    ]
+    ],
+    "msteams": {
+        "entities": [
+            {
+                "type": "mention",
+                "text": "<at>$COMMIT_AUTHOR_NAME</at>",
+                "mentioned": {
+                  "id": "$COMMIT_AUTHOR_MAIL",
+                  "name": "$COMMIT_AUTHOR_NAME"
+                }
+              }
+        ]
+    },
+    "\$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+    "version": "1.5"
 }
 END_HEREDOC
 )
